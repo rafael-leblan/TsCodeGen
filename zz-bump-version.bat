f5ef4ee080c09bad3f988af0e1776af1834ff929
rafael-leblan <rafael.leblan@gmail.com>
2018-05-27 16:59:02 -0400

    fix bump script

--- zz-bump-version.bat
+++ zz-bump-version.bat
@@ -1,5 +1,5 @@
 python ./nuget/bump-version-update-patch.py
-git am -3 ./nuget/.bump-version/RafaelSoft.TsCodeGen-bumpVersion.patch
+git am -3 --ignore-space-change --ignore-whitespace ./nuget/.bump-version/RafaelSoft.TsCodeGen-bumpVersion.patch
 git checkout --theirs .
 git add *
 git am --continue
