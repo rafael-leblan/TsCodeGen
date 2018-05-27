8dd1c29dc0b8695b359f0ef3a0bd187c76dc6739
rafael-leblan <rafael.leblan@gmail.com>
2018-05-27 16:25:12 -0400

    NUGET 1.0.2

--- zz-push-nuget.bat
+++ zz-push-nuget.bat
@@ -1,4 +1,4 @@
 nuget pack nuget/RafaelSoft.TsCodeGen.nuspec
 nuget pack nuget/RafaelSoft.TsCodeGen.WebApi.nuspec
-start nuget push RafaelSoft.TsCodeGen.1.0.1.nupkg -source https://www.nuget.org
-start nuget push RafaelSoft.TsCodeGen.WebApi.1.0.1.nupkg -source https://www.nuget.org
+start nuget push RafaelSoft.TsCodeGen.1.0.2.nupkg -source https://www.nuget.org
+start nuget push RafaelSoft.TsCodeGen.WebApi.1.0.2.nupkg -source https://www.nuget.org
